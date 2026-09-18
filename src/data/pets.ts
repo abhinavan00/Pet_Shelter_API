e69ab688ee645debc13a6fee641df3f3a63b84b1
Abhinav Anand <abhinavanand072@gmail.com>
2026-09-18 09:33:25 +0530

    responding with data

--- src/data/pets.ts
+++ src/data/pets.ts
@@ -1,4 +1,21 @@
-export const pets = [
+export type Pet = {
+    id: number,  
+    name: string,  
+    species: string,  
+    breed: string,  
+    age: number,  
+    adopted: boolean,  
+    intakeDate: Date,  
+    adoptionDate?: Date,
+    medicalRecord: {  
+        vaccinations: string[],  
+        weightKg: number,  
+        microchipId: null | string 
+    },  
+    photo: string
+}
+
+export const pets: Pet[] = [
     {  
         id: 1,  
         name: "Bella",  
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import express, {type Express, type Request, type Response} from "express";
+import { pets } from "./data/pets.ts";
 
 const PORT = 8000
 const app: Express = express()
 
 app.get('/', (req: Request, res: Response) => {
-    res.json('Hello World!')
+    res.json(pets)
     console.log('Hello from frontEnd!')
 })
 
