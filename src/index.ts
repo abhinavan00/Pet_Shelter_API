208c016449ac818fa70febdfae24ce645375ecc0
Abhinav Anand <abhinavanand072@gmail.com>
2026-09-18 09:38:33 +0530

    added cors package

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import express, {type Express, type Request, type Response} from "express";
+import cors from 'cors';
 import { pets } from "./data/pets.ts";
 
 const PORT = 8000
 const app: Express = express()
 
+app.use(cors())
+
 app.get('/', (req: Request, res: Response) => {
     res.json(pets)
-    console.log('Hello from frontEnd!')
 })
 
 app.listen(PORT, () => {
