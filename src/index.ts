d8f5acb3a11b0aeadfce486596ce8cb2f1cb3bc1
Abhinav Anand <abhinavanand072@gmail.com>
2026-09-18 09:55:31 +0530

    Responding for a specific request with id

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,13 @@ app.get('/', (req:Request, res:Response<Pet[]>):void => {
     res.json(pets)
 })
 
+app.get('/:id', (req:Request, res:Response<Pet>):void => {
+    const { id } = req.params
+
+    const matchpet = pets.find(pet => pet.id === Number(id))
+    res.json(matchpet)
+})
+
 app.use((req:Request, res:Response<{message:string}> ):void => {
     res.status(404).json({message: 'Route not found!'})
 })
