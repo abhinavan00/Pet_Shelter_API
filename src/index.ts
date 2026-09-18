e0ce4196d1ffde615f2e1a97c82fc1ff151cee61
Abhinav Anand <abhinavanand072@gmail.com>
2026-09-18 09:46:59 +0530

    made res type more specific

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 import express, {type Express, type Request, type Response} from "express";
 import cors from 'cors';
-import { pets } from "./data/pets.ts";
+import { pets, type Pet } from "./data/pets.ts";
 
 const PORT = 8000
 const app: Express = express()
 
 app.use(cors())
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', (req:Request, res:Response<Pet[]>):void => {
     res.json(pets)
+})
+
+app.use((req:Request, res:Response<{message:string}> ):void => {
+    res.status(404).json({message: 'Route not found!'})
 })
 
 app.listen(PORT, () => {
